b3617c704502bb9461b36f3e9a86c95b6de8ce06
nsrchemie <nsrchemie@gmail.com>
2017-04-13 21:50:25 -0400

    initial set-up;

---
+++ hello_bot/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+
+const token = '';
+
+let slack = new RtmClient(token, {
+  logLevel: 'debug',
+  dataStore: new MemoryDataStore(),
+  autoReconnect: true,
+  autoMark: true
+});
+
+slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () =>
+{ let user = slack.dataStore.getUserById(slack.activeUserId);
+  let team = slack.dataStore.getTeamById(slack.activateTeamId);
+
+console.log(`Connected to ${team.name} as ${user.name}`);
+});
+
+slack.start();
