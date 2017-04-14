388f3784e443e91892f9426db743bb6b8742e48c
nsrchemie <nsrchemie@gmail.com>
2017-04-13 22:59:35 -0400

    pulling info of channels logged into

--- hello_bot/index.js
+++ hello_bot/index.js
@@ -7,7 +7,7 @@ const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const token = require('../firstapi.js').pass;
 
 let slack = new RtmClient(token, {
-  logLevel: 'debug',
+  logLevel: 'error',
   dataStore: new MemoryDataStore(),
   autoReconnect: true,
   autoMark: true
@@ -17,7 +17,27 @@ slack.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () =>
 { let user = slack.dataStore.getUserById(slack.activeUserId);
   let team = slack.dataStore.getTeamById(slack.activateTeamId);
 
-console.log(`Connected to as ${user.name}`);
+let channels = getChannels(slack.dataStore.channels);
+
+let channelNames = channels.map((channel) => {
+ return channel.name;
+}).join(', ');
+
+console.log(`Currently in ${channelNames} under the name ${user.name}`);
 });
 
 slack.start();
+
+function getChannels(allChannels) {
+ let channels = [];
+
+for (let id in allChannels) {
+let channel = allChannels[id];
+
+if (channel.is_member) {
+ channels.push(channel);
+ }
+}
+
+return channels;
+}
