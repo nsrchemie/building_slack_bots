88383e82f1fc6cda3db11866e65013742c385ea6
nsrchemie <nsrchemie@gmail.com>
2017-04-15 00:42:52 -0400

    added simple roll dice game

--- scnd_bot/index.js
+++ scnd_bot/index.js
@@ -12,3 +12,33 @@ const bot = new Bot({
 bot.respondTo('hello', (message, channel, user) => {
   bot.send(`Hello to you too, ${user.name}!`, channel)
 }, true);
+
+bot.respondTo('roll', (message,channel,user) => {
+  let args = getArgs(message.text);
+
+let firstRoll = Math.round(Math.random() * 100);
+let secondRoll = Math.round(Math.random() * 100);
+
+let challenger = user.name;
+let opponent = args[0];
+
+while (firstRoll === secondRoll) {
+  secondRoll = Math.round(Math.random() * 100);
+}
+
+let winner = firstRoll > secondRoll ? challenger : opponent;
+
+bot.send(
+ `${challenger} fancies their chances against ${opponent}!\n
+  ${challenger} rolls: ${firstRoll}\n
+  ${opponent} rolls: ${secondRoll}\n\n
+  *${winner} is the winner!*`,
+  channel);
+
+}, true);
+
+function getArgs(msg) {
+  return msg.split(' ').slice(1);
+}
+
+
